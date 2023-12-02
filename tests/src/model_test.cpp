10590833197ac83ec79706958541545538de6d3a
Manuel <m-tischhauser@hotmail.com>
2023-12-02 21:10:15 +0100

    tests added

--- tests/src/model_test.cpp
+++ tests/src/model_test.cpp
@@ -7,13 +7,28 @@ import Model;
 #include "doctest.h"
 
 TEST_CASE("Model") {
-    auto model = ann::Model{ann::layers::Dense<float, 3, 5, ann::activations::Relu>{},
-                            ann::layers::Dense<float, 5, 4, ann::activations::Relu>{},
-                            ann::layers::Dense<float, 4, 2, ann::activations::Softmax>{}};
+    linalg::Matrix<float, 3, 2> W1{{-2.2,  0.1},
+                                   { 1,    0.7},
+                                   { 1.5, -1.3}};
 
-    linalg::Matrix<float, 3, 1> X{};
+    linalg::Matrix<float, 3, 1> B1{{-1},
+                                   { 1},
+                                   {-1}};
+
+    linalg::Matrix<float, 2, 3> W2{{-4.2, 0.4,  1},
+                                   { 2.2, 0,   -1}};
+
+    linalg::Matrix<float, 2, 1> B2{{-0.1},
+                                   { 0.1}};
+
+    auto model = ann::Model{ann::layers::Dense<float, 2, 3, ann::activations::Relu>{W1, B1},
+                            ann::layers::Dense<float, 3, 2, ann::activations::Softmax>{W2, B2}};
+
+    linalg::Matrix<float, 2, 1> X{{1.1},
+                                  {2.2}};
 
     auto Y = model.predict(X);
 
-    CHECK(false);
+    CHECK(doctest::Approx(0.7783367616) == Y(0, 0));
+    CHECK(doctest::Approx(0.2216632384) == Y(1, 0));
 }
